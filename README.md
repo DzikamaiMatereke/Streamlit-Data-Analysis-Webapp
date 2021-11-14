559e198b45be58a3a40e8af4d4a1d81fd35f1468
DzikamaiM <22172609+DzikamaiMatereke@users.noreply.github.com>
2021-11-14 15:04:35 +1100

    Changes to be committed: 	new file:   .dockerignore 	modified:   README.md 	new file:   app/.dockerignore 	new file:   app/.google-cookie 	new file:   app/Dockerfile 	new file:   app/app.py 	new file:   app/requirements.txt 	new file:   docker-compose-dev.yml 	new file:   docker-compose.yml 	new file:   nginx/Dockerfile 	new file:   nginx/project.conf

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Streamlit-Data-Analysis-Webapp
+Creating a simple web application for calculating savings rates
+The application will use streamlit for the front end and nginx for the web server and reverse proxy and docker for containerisation
---
+++ app/app.py
@@ -0,0 +1,12 @@
+#This application is built by dzikamai matereke
+#Github: 
+#linkedin:  
+#
+
+'''this is build using the following libraries: 
+streamlit: https://streamlit.io/ an opem source platform for creating and sharing custom web apps for machine learning and data science with python.
+plotly.graph_objects: https://plotly.com/python-api-reference/generated/plotly.graph_objects.Bar.html#id5 a graphing library for creating charts and graphics./
+'''
+import streamlit as st
+import plotly.graph_objects as go
+import numpy as np
