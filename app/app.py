0d63f891d6d27a2e867538176b22171e67718b40
DzikamaiMatereke <22172609+DzikamaiMatereke@users.noreply.github.com>
2021-11-20 20:27:07 +1100

    Update app.py
    created navigation bar

--- app/app.py
+++ app/app.py
@@ -13,21 +13,23 @@ import numpy as np
 import pandas as pd
 import requests
 
+
 def main():
     
     st.title("Analysis of Health Care costs borne by Health Co.")   
     try:
         #Will update this to use an S3 bucket to pull data from
         #r = requests.get('https://streamlitdatabucket.s3.ap-southeast-2.amazonaws.com/health.xlsx?AWSAccessKeyId=AKIASLYPYXU7MNLRRE2V&Expires=1637390104&Signature=p22bHaDj19fKpGqXSaSdj9dfhyk%3D')
+       
         df = pd.read_excel('C:\\Users\\deema\\OneDrive\\Documents\\GitHub\\Streamlit-Data-Analysis-Webapp\\datafile\\health.xlsx')
         st.dataframe(df)
-    finally:
+    except:
         st.write("Import function has failed")
 
     
 
     add_selectbox = st.sidebar.selectbox(
-        "How would you like to be contacted?",
+        "Navigation",
         ("Home", "Descriptive Analytics", "Predictice Analytics")
     )
 if __name__=='__main__':
